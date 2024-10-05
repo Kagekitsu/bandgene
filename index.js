60f2f156643aad073b619f23f6fca128bbf5b7d8
Braden <baj147@yahoo.com>
2024-10-05 00:02:46 -0400

    added header and footer ejs to index.ejs and updated header.ejs to link stylesheet

--- index.js
+++ index.js
@@ -27,7 +27,8 @@ app.post("/submit", (req, res) => {
   //scroll down to see the two arrays.
   //2. Send the index.ejs as a response and add the adjective and noun to the res.render
   //3. Test to make sure that the random words display in the h1 element in index.ejs
-  
+  const bandName = adj.random() + noun.random();
+  res.render("index.ejs", {bandNameGene: bandName});
 });
 
 app.listen(port, () => {
