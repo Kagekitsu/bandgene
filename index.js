e57bb4c14595cc88c6c5717d5f345178f82fff5f
Braden <baj147@yahoo.com>
2024-10-05 01:03:34 -0400

    added a space between adj and now

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.post("/submit", (req, res) => {
   //scroll down to see the two arrays.
   //2. Send the index.ejs as a response and add the adjective and noun to the res.render
   //3. Test to make sure that the random words display in the h1 element in index.ejs
-  const bandName = adj[(Math.floor(Math.random() * (adj.length)))] + noun[(Math.floor(Math.random() * (noun.length)))];
+  const bandName = adj[(Math.floor(Math.random() * (adj.length)))] + " " + noun[(Math.floor(Math.random() * (noun.length)))];
   res.render("index.ejs", {bandNameGene: bandName});
 });
 
